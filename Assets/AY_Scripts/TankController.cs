7ac06ba8713e54cec0badea10147960d1b08919b
AihasasYadu <AihasasYadu@users.noreply.github.com>
2020-10-22 03:12:21 +0530

    Commit Includes - Scene Setup Generic Singleton Class Tank Service Singleton Tank Controller Script

---
+++ Assets/AY_Scripts/TankController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public float speed = 10;
+
+    private Joystick joystick;
+    private float horizontalMove;
+    private float verticalMove;
+    
+    void Awake()
+    {
+        joystick = FindObjectOfType<FixedJoystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = joystick.Horizontal * speed;
+        verticalMove = joystick.Vertical * speed;
+        if (joystick.Horizontal > 0.2f)
+        {
+            horizontalMove = 1 * speed;
+        }
+        else if (joystick.Horizontal < -0.2f)
+        {
+            horizontalMove = -1 * speed;
+        }
+        else
+        {
+            horizontalMove = 0;
+        }
+        Vector3 pos = transform.position;
+        pos.x += horizontalMove * Time.deltaTime;
+        pos.z += verticalMove * Time.deltaTime;
+        transform.position = pos;
+    }
+}
---
+++ Assets/AY_Scripts/TankService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankService : MonoSingletonGeneric<TankService>
+{
+    public GameObject tank;
+    void Start()
+    {
+        TankService.Instance.GetTank();
+    }
+
+    private void GetTank()
+    {
+        Instantiate(tank);
+    }
+}

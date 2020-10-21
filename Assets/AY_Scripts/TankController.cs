f1eff3900bea3b977dd9dfe3a1fd31248d96f9fa
AihasasYadu <AihasasYadu@users.noreply.github.com>
2020-10-22 04:51:02 +0530

    Additions - Tank Looks in the Direction of the Joystick A few Box Colliders

--- Assets/AY_Scripts/TankController.cs
+++ Assets/AY_Scripts/TankController.cs
@@ -7,34 +7,28 @@ public class TankController : MonoBehaviour
     public float speed = 10;
 
     private Joystick joystick;
+    private Transform tankParent;
     private float horizontalMove;
     private float verticalMove;
+    private Vector3 lookDirection;
     
     void Awake()
     {
         joystick = FindObjectOfType<FixedJoystick>();
+        tankParent = transform.parent;
     }
-
-    // Update is called once per frame
     void Update()
     {
         horizontalMove = joystick.Horizontal * speed;
         verticalMove = joystick.Vertical * speed;
-        if (joystick.Horizontal > 0.2f)
-        {
-            horizontalMove = 1 * speed;
-        }
-        else if (joystick.Horizontal < -0.2f)
-        {
-            horizontalMove = -1 * speed;
-        }
-        else
-        {
-            horizontalMove = 0;
-        }
-        Vector3 pos = transform.position;
+        Vector3 pos = tankParent.position;
         pos.x += horizontalMove * Time.deltaTime;
         pos.z += verticalMove * Time.deltaTime;
-        transform.position = pos;
+        tankParent.position = pos;
+        if (joystick.Direction.x != 0 && joystick.Direction.y != 0)
+        {
+            lookDirection = new Vector3(joystick.Direction.x, 0, joystick.Direction.y);
+        }
+        transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
     }
 }

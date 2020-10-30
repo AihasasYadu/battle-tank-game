59a441d0e5cd8155083677be30982b39413fb885
AihasasYadu <AihasasYadu@users.noreply.github.com>
2020-10-30 20:40:37 +0530

    Added : Shell Service Scene Index Script

---
+++ Assets/AY_Scripts/ShellService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellService : MonoSingletonGeneric<ShellService>
+{
+    [SerializeField] private ShellController shell;
+
+    public void GetShell(int dmg, float speed)
+    {
+        Transform temp = TankService.Instance.GetShellPos.transform;
+        ShellController shellInstance = Instantiate(shell, temp.position, temp.rotation);
+        shellInstance.Initialize(dmg, speed);
+    }
+}
--- Assets/AY_Scripts/TankService.cs
+++ Assets/AY_Scripts/TankService.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankService : MonoSingletonGeneric<TankService>
 {
-    public TankController tank;
+    [SerializeField] private TankController tank;
     [SerializeField] private Joystick joystick;
-    public GameObject shellPosition;
-    public List<TankTypesScriptable> type;
+    [SerializeField] private GameObject shellPosition;
+    [SerializeField] private Button shootButton;
+    [SerializeField] private List<TankTypesScriptable> type;
+
+    public GameObject GetShellPos { get { return shellPosition; } }
+    public GameObject SetShellPos { set {
+                                            Vector3 pos = shellPosition.transform.position;
+                                            pos.x = value.transform.position.x;
+                                            pos.z = value.transform.position.z;
+                                            shellPosition.transform.position = pos;
+                                            shellPosition.transform.rotation = value.transform.rotation;
+                                        } }
     void Start()
     {
-        TankService.Instance.GetTank();
+        GetTank();
     }
 
     private void GetTank()
     {
-        int playerChoice = PlayerPrefs.GetInt("Player's Choice");
-        Instantiate(tank).Initialize(type[playerChoice], joystick);
+        int playerChoice = PlayerPrefs.GetInt("TankSelected");
+        Instantiate(tank).Initialize(type[playerChoice], joystick, shootButton);
     }
 }
